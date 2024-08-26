1db7a022300bc5065eeaca82855920803fe97f23
SimoneDUrso <simone.durso98@gmail.com>
2024-08-26 12:42:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+MILESTONE 0:
+1 - Creare la struttura base con lo store;
+2 - Inserisco gli endpoint all'interno 
+
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { Store } from "./Store";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      Store,
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    ciao
+  </div>
+</template>
+<style lang="scss">
+@use "./style/Generals.scss"
+</style>
